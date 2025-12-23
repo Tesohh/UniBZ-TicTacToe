4915772d3dbc15e6572bede0b863322dec1a4b62
Simone Tesini <simozilla006@gmail.com>
2025-12-23 18:54:01 +0100

    add scoring and all

--- Main.java
+++ Main.java
@@ -16,10 +16,14 @@ or her work.
 * -s: human vs (smart bot)
 * -m: human vs (smart memory bot)
 * -S: human vs (very smart bot)
-* -S-{percentage}: human vs (very smart bot) with specified 0-100 difficulty (0% difficulty = dumb bot) 
+* -S-{percentage}: human vs (very smart bot) with specified 0-100 difficulty 
+* (0% difficulty = dumb bot, 100% difficulty = almost unbeatable)
 *
 * specify two flags to pick both players (eg. smart bot vs very smart bot at 20% difficulty = -s -S-20)
 */
+
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         Player player1;
@@ -42,17 +46,53 @@ class Main {
                 ANSI.FG_BLACK + ANSI.BG_RED, player2.getName(), ANSI.RESET);
         System.out.println();
 
+        var scanner = new Scanner(System.in);
         int player1Wins = 0;
         int player2Wins = 0;
 
         while (true) { // every iteration is a new game
             var game = new Game(player1, player2);
-            // TODO ask for input...
+            if (player1Wins > 0 || player2Wins > 0) {
+                System.out.printf("Score:  %s %d : %d %s\n", Mark.X.prettyStringBG(), player1Wins, player2Wins,
+                        Mark.O.prettyStringBG());
+            }
+            System.out.printf("%s%s P %slay  %s%s Q %suit  %s%s I %snstructions\n",
+                    ANSI.FG_BLACK, ANSI.BG_WHITE, ANSI.RESET, ANSI.FG_BLACK, ANSI.BG_WHITE, ANSI.RESET, ANSI.FG_BLACK,
+                    ANSI.BG_WHITE, ANSI.RESET);
+            System.out.print("> ");
+
+            // use a full line instead of just a byte so that users can also type "play" or
+            // "instructions" or "quit" or whatever
+            var input = scanner.nextLine().toLowerCase();
+
+            if (input.length() >= 1) {
+                if (input.charAt(0) == 'q') {
+                    scanner.close();
+                    return; // close the game..
+                } else if (input.charAt(0) == 'i') {
+                    System.out.println(instructions);
+                    continue;
+                } else if (input.charAt(0) != 'p') {
+                    System.out.println("invalid command, try again!");
+                    continue;
+                }
+            }
+
+            // in any other case (user just pressed enter, or pressed p), play the game..
+
             while (true) { // every iteration is a new turn
                 game.display();
                 var situation = game.nextTurn();
-                if (situation != Game.Situation.NOTHING) {
-                    System.out.println("dbg: " + situation);
+                if (situation == Game.Situation.PLAYER1_WIN) {
+                    player1Wins += 1;
+                    System.out.printf("%s wins!\n\n", Mark.X.prettyStringBG());
+                    break;
+                } else if (situation == Game.Situation.PLAYER2_WIN) {
+                    player2Wins += 1;
+                    System.out.printf("%s wins!\n\n", Mark.O.prettyStringBG());
+                    break;
+                } else if (situation == Game.Situation.DRAW) {
+                    System.out.printf("%s%sDraw!%s\n\n", ANSI.FG_BLACK, ANSI.BG_WHITE, ANSI.RESET);
                     break;
                 }
                 System.out.println();
@@ -86,4 +126,6 @@ class Main {
             return new HumanPlayer();
         }
     }
+
+    static String instructions = String.format("TODO TODO");
 }
